7c6af9e60e772c63273fafb8e3cf643631c0397c
alex800121 <alex800121@hotmail.com>
2024-02-07 10:31:37 +0800

    day9

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,6 +1,32 @@
 module Day8 where
 
+import Data.Char
+import MyLib (Parser, signedInteger)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.List (foldl')
+import Data.Maybe (fromMaybe)
+import Data.Bifunctor (Bifunctor(..))
+
+readSlash :: Parser Char
+readSlash = string "\\\\" >> pure '\\'
+
+readQuote :: Parser Char
+readQuote = string "\\\"" >> pure '\"'
+
+readHex :: Parser Char
+readHex = string "\\x" >> f <$> count 2 (satisfy isHexDigit)
+  where
+    f = chr . foldl' (\acc x -> digitToInt x + (16 * acc)) 0
+
+readASCII :: Parser String
+readASCII = char '\"' >> many (readSlash <|> readQuote <|> readHex <|> anySingleBut '\"') <* char '\"'
+
+encodeASCII :: String -> String
+encodeASCII = show
 day8 :: IO ()
 day8 = do
-  -- input <- readFile "input/input8.txt"
-  return ()
+  input <- lines <$> readFile "input/input8.txt"
+  -- input <- lines <$> readFile "input/test8.txt"
+  print $ uncurry (-) $ bimap sum sum $ unzip $ map ((,) <$> length <*> length . fromMaybe "" . parseMaybe readASCII) input
+  print $ uncurry subtract $ bimap sum sum $ unzip $ map ((,) <$> length <*> length . encodeASCII) input
