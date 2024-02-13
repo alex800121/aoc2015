703c1917585034d569894d06d26a5e26b23b4e23
alex800121 <alex800121@hotmail.com>
2024-02-13 22:03:28 +0800

    day23

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,7 +1,24 @@
 module Day23 where
 
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import MyLib
+import Text.Megaparsec (parseMaybe)
+import Data.Maybe (fromJust)
+
+type S = (Int, (Int, Int))
+
+type Ins = Vector [String]
+
+readIns :: Ins -> S -> S
+readIns v s@(p, (a, b)) = case v V.!? p of
+  Nothing -> s
+  Just ["jio", x, y] -> undefined
+  where
+    readReg x = if head x == 'a' then fst else snd
+    readInt = fromJust . parseMaybe signedInteger
 
 day23 :: IO ()
 day23 = do
-  -- input <- readFile "input/input23.txt"
-  return ()
+  input <- V.fromList . map words . lines <$> readFile "input/input23.txt"
+  print input
