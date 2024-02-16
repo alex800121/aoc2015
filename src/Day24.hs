cf3702c0010f03367b57a2516a41e6fe94c2d197
alex800121 <alex800121@hotmail.com>
2024-02-16 08:33:10 +0800

    done

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,6 +1,30 @@
 module Day24 where
 
+import Control.Monad (guard)
+import Data.Array
+import Data.List (delete, sort, sortBy, uncons, insert, minimumBy)
+import Data.Maybe (maybeToList)
+import MyLib (pickAnySplit)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Function (on)
+
+
+sumV :: Int -> Int -> [Int] -> [[Int]]
+sumV s n _ | s < 0 || n < 0 = []
+sumV 0 _ _ = [[]]
+sumV _ _ [] = []
+sumV m n (x : xs)
+  | x <= m = map (x :) (sumV (m - x) (n - 1) xs) <> sumV m n xs
+  | otherwise = sumV m n xs
+
+
 day24 :: IO ()
 day24 = do
-  -- input <- readFile "input/input24.txt"
-  return ()
+  input <- sort . map (read @Int) . lines <$> readFile "input/input24.txt"
+  let s = sum input `div` 3
+      l = length input `div` 3
+      s' = sum input `div` 4
+      l' = length input `div` 4
+  print $ product $ minimumBy (\x y -> on compare length x y <> on compare product x y) $ sumV s l input
+  print $ product $ minimumBy (\x y -> on compare length x y <> on compare product x y) $ sumV s' l' input
--- src/Day25.hs
+++ src/Day25.hs
@@ -1,6 +1,15 @@
 module Day25 where
 
+input = (3029, 2947)
+
+go target t@(x, y) i
+  | target == t = i
+  | y == 1 = go target (1, x + 1) i'
+  | otherwise = go target (x + 1, y - 1) i'
+  where
+    i' = (i * 252533) `mod` 33554393
+
 day25 :: IO ()
 day25 = do
   -- input <- readFile "input/input25.txt"
-  return ()
+  print $ go input (1, 1) 20151125
